d3675ced8907b9dcb2018bd0ba64d9a18950bd84
cbl6499 <clemens.blecha@students.fhv.at>
2017-12-18 20:37:38 +0100

    ejb update

--- src/java/ManagedSearchBean.java
+++ src/java/ManagedSearchBean.java
@@ -1,53 +1,98 @@
 import java.util.ArrayList;
-import javax.annotation.ManagedBean;
+import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.SessionScoped;
 
-
-@ManagedBean
+@ManagedBean (name = "managedSeachBean")
+@SessionScoped
 public class ManagedSearchBean {
 
     @EJB
     private RemoteSearchBeanFace remoteSearchBean;
+
+    public ArrayList<BookDTO> books;
+    public ArrayList<DvdDTO> dvds;
+    public ArrayList<MagazineDTO> magazines;
+    public ArrayList<BookDTO> isbnBooks;
+    public ArrayList<DvdDTO> titleDvds;
+    public ArrayList<MagazineDTO> titleMagazins;
 
     public ManagedSearchBean(){
 
         Properties p = new Properties();
 
         p.put("java.naming.factory.initial","org.jpn.interfaces.NamingContextFactory");
-        p.put("java.naming.provider.url","jpn://localhost:1099");
+        p.put("java.naming.provider.url","jpn://localhost:3700");
         p.put("java.naming.factory.url.pkgs","org.jboss.naming:org.jpn.interfaces");
 
         InitialContext cfx = new InitialContext(p);
 
-        remoteSearchBean = (RemoteSeachBeanFace) cfx.lookup("");
+        remoteSearchBean = (RemoteSeachBeanFace) cfx.lookup("SearchEJB");
     }
 
-    public ArrayList<ArrayList<DTO>> search(String searchTerm) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void search(String searchTerm){
+      ArrayList<ArrayList<DTO>> allMedias = remoteSearchBean.search(searchTerm);
 
-        ArrayList<ArrayList<DTO>> allMedias = remoteSearchBean.search(searchTerm);
-
-        ArrayList<DTO> bookArrayList= allMedias.get(0);
-        ArrayList<DTO> dvdArrayList = allMedias.get(1);
-        ArrayList<DTO> magazineArrayList = allMedias.get(2);
-
-        return allMedias;
+      books = allMedias.get(0);
+      dvds = allMedias.get(1);
+      magazines = allMedias.get(2);
     }
 
-
-    public ArrayList<BookDTO> getBooksByISBN(String isbn) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return remoteSearchBean.getBookByISBN(isbn);
+    public void getBooksByISBN(String isbn){
+      isbnBooks = remoteSearchBean.getBooksByISBN(isbn);
     }
 
-
-    public ArrayList<DvdDTO> getDvdByTitle(String title) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return remoteSearchBean.getDvdByTitle(title);
+    public void getDvdByTitle(String title){
+      titleDvd = remoteSearchBean.getDvdByTitle(title);
     }
 
-
-    public ArrayList<MagazineDTO> getMagazinesByTitleAndEdition(String title, String edition) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        return remoteSearchBean.getMagazinesByTitleAndEdition(title, edition);
+    public void getMagazinesByTitleAndEdition(String title, String edition){
+      titleMagazins = remoteSearchBean.getMagazinesByTitleAndEdition(title, edition);
     }
-}
+
+	public ArrayList<BookDTO> getBooks() {
+		return books;
+	}
+
+	public void setBooks(ArrayList<BookDTO> books) {
+		this.books = books;
+	}
+
+	public ArrayList<DvdDTO> getDvds() {
+		return dvds;
+	}
+
+	public void setDvds(ArrayList<DvdDTO> dvds) {
+		this.dvds = dvds;
+	}
+
+	public ArrayList<MagazineDTO> getMagazines() {
+		return magazines;
+	}
+
+	public void setMagazines(ArrayList<MagazineDTO> magazines) {
+		this.magazines = magazines;
+	}
+
+	public ArrayList<BookDTO> getIsbnBooks() {
+		return isbnBooks;
+	}
+
+	public void setIsbnBooks(ArrayList<BookDTO> isbnBooks) {
+		this.isbnBooks = isbnBooks;
+	}
+
+	public ArrayList<DvdDTO> getTitleDvds() {
+		return titleDvds;
+	}
+
+	public void setTitleDvds(ArrayList<DvdDTO> titleDvds) {
+		this.titleDvds = titleDvds;
+	}
+
+	public ArrayList<MagazineDTO> getTitleMagazins() {
+		return titleMagazins;
+	}
+
+	public void setTitleMagazins(ArrayList<MagazineDTO> titleMagazins) {
+		this.titleMagazins = titleMagazins;
+	}
