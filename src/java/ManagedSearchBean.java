d41cc76adf013ad9b3b2ad0346cde76a5c938c25
cbl6499 <clemens.blecha@students.fhv.at>
2017-12-19 10:38:39 +0100

    ejb update tuat aber immer no ned

--- src/java/ManagedSearchBean.java
+++ src/java/ManagedSearchBean.java
@@ -1,41 +1,78 @@
 import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.enterprise.context.SessionScoped;
-
+import java.io.Serializable;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.ejb.EJB;
 
 @ManagedBean (name = "managedSeachBean")
-
-public class ManagedSearchBean{
+@SessionScoped
+public class ManagedSearchBean implements Serializable{
 
     @EJB
     private RemoteSearchBeanFace remoteSearchBean;
 
-    public ArrayList<BookDTO> books;
-    public ArrayList<DvdDTO> dvds;
-    public ArrayList<MagazineDTO> magazines;
-    public ArrayList<BookDTO> isbnBooks;
-    public ArrayList<DvdDTO> titleDvds;
-    public ArrayList<MagazineDTO> titleMagazins;
+    private ArrayList<BookDTO> books;
+    private ArrayList<DvdDTO> dvds;
+    private ArrayList<MagazineDTO> magazines;
+    private ArrayList<BookDTO> isbnBooks;
+    private ArrayList<DvdDTO> titleDvds;
+    private ArrayList<MagazineDTO> titleMagazins;
+    private int focus;
+    private Boolean foundBooks;
+    private Boolean foundDvds;
+    private Boolean foundMagazines;
 
     public ManagedSearchBean(){
 
         Properties p = new Properties();
 
-        p.put("java.naming.factory.initial","org.jpn.interfaces.NamingContextFactory");
-        p.put("java.naming.provider.url","jpn://localhost:3700");
-        p.put("java.naming.factory.url.pkgs","org.jboss.naming:org.jpn.interfaces");
+        //p.put("java.naming.factory.initial","org.jpn.interfaces.NamingContextFactory");
+        //p.put("java.naming.provider.url","jpn://localhost:3700");
+        //p.put("java.naming.factory.url.pkgs","org.jboss.naming:org.jpn.interfaces");
 
-        InitialContext cfx = new InitialContext(p);
-
-        remoteSearchBean = (RemoteSeachBeanFace) cfx.lookup("SearchEJB");
+        p.put("org.omg.CORBA.ORBInitialHost", "localhost");
+        p.put("org.omg.CORBA.ORBInitialPort", "3700");
+        p.put("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
+        p.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
+        p.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            
+        InitialContext cfx;
+        try {
+            cfx = new InitialContext(p);
+             remoteSearchBean = (RemoteSearchBeanFace) cfx.lookup("SearchEJB");
+        } catch (NamingException ex) {
+            Logger.getLogger(ManagedSearchBean.class.getName()).log(Level.SEVERE, null, ex);
+        }       
     }
 
     public void search(String searchTerm){
       ArrayList<ArrayList<DTO>> allMedias = remoteSearchBean.search(searchTerm);
 
-      books = allMedias.get(0);
-      dvds = allMedias.get(1);
-      magazines = allMedias.get(2);
+      ArrayList<DTO> booksFound = allMedias.get(0);
+      ArrayList<DTO> dvdsFound = allMedias.get(1);
+      ArrayList<DTO> magazinesFound = allMedias.get(2);
+      
+      for(int i = 0; i<booksFound.size(); i++){
+          BookDTO book = (BookDTO) booksFound.get(i);
+          books.add(book);
+      }
+      
+      for(int i = 0; i<dvdsFound.size(); i++){
+          DvdDTO dvd = (DvdDTO) dvdsFound.get(i);
+          dvds.add(dvd);
+      }
+      
+      for(int i = 0; i<magazinesFound.size(); i++){
+          MagazineDTO magazine = (MagazineDTO) magazinesFound.get(i);
+          magazines.add(magazine);
+      }
+      
+      
     }
 
     public void getBooksByISBN(String isbn){
@@ -97,4 +134,46 @@ public class ManagedSearchBean{
 	public void setTitleMagazins(ArrayList<MagazineDTO> titleMagazins) {
 		this.titleMagazins = titleMagazins;
 	}
+        
+        public void setFocus(){
+        }
+        
+        public int getFocus(){
+            if(books.isEmpty()){
+                    focus = 1;
+                    if(dvds.isEmpty()){
+                        focus = 2;
+                        if(magazines.isEmpty()){
+                            focus = 0;
+                        }
+                    }
+            }else if(!books.isEmpty()){
+                focus = 0;
+            }
+            return focus;
+        }
+        
+        public void setFoundBooks(Boolean found){
+            foundBooks = found;
+        }
+        
+        public Boolean getFoundBooks(){
+            return foundBooks;
+        }
+        
+        public void setFoundDvds(Boolean found){
+            foundDvds = found;
+        }
+        
+        public Boolean getFoundDvds(){
+            return foundDvds;
+        }
+        
+        public void setFoundMagazines(Boolean found){
+            foundMagazines = found;
+        }
+        
+        public Boolean getFoundMagazines(){
+            return foundMagazines;
+        }
 }
